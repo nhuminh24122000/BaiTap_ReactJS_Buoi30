f890217f3431f6ae837736f805c19cf200d8c48e
MinhTran <nhuminh24122000@gmail.com>
2023-04-08 05:52:28 +0700

    Initialize project using Create React App

---
+++ src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
+
+
+function BaiTapThucHanhLayout() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+        
+    )
+}
+
+export default BaiTapThucHanhLayout
---
+++ src/BaiTapLayoutComponent/Body.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Banner from './Banner'
+import Card from './Card'
+
+function Body() {
+  return (
+    <div>
+        <Banner />
+        <Card />
+    </div>
+    
+  )
+}
+
+export default Body
