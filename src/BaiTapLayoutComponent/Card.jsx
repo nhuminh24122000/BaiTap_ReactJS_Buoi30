93cbb170bc366c46657084d89da60573d871f6b9
MinhTran <nhuminh24122000@gmail.com>
2023-04-08 08:01:33 +0700

    fix code

--- src/BaiTapLayoutComponent/Card.jsx
+++ src/BaiTapLayoutComponent/Card.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Styles from './style.css';
-
 function Card() {
   return (
     <section className="pt-4">
--- src/BaiTapLayoutComponent/Header.jsx
+++ src/BaiTapLayoutComponent/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Styles from './style.css';
-
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
